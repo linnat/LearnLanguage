4276793fe00fe3b515928b36dd22f9167e724040
Ray_Lin <Ray_Lin@DESKTOP-9MGDSK7>
2022-01-13 16:33:11 -0500

    Add project files.

---
+++ LearnLanguage/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LearnLanguage
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LanguageTeacher teacher = new LanguageTeacher();
+            teacher.AddLanguage("English");
+
+            LanguageStudent student = new LanguageStudent();
+            teacher.Teach(student, "English");
+            teacher.Teach(student, "French");
+
+            foreach (var language in student.Languages)
+            {
+                Console.WriteLine(language);
+            }
+        }
+    }
+
+    public class LanguageStudent
+    {
+        private List<string> languages = new List<string>();
+        public List<string> Languages
+        {
+            get { return languages; }
+            set { languages = value; }
+        }
+
+        public bool isStudent = false;
+        public bool isTeach = false;
+
+        public LanguageStudent()
+        {
+            isStudent = true;
+            isTeach = false;
+        }
+
+        public void AddLanguage(string language)
+        {
+            if (!isStudent || isTeach)
+            {
+                var existLanguages = languages.Any(x => languages.Contains(language));
+                if (!existLanguages)
+                {
+                    languages.Add(language);
+                }
+                isTeach = false;
+            }
+        }
+    }
+
+    public class LanguageTeacher : LanguageStudent
+    {
+        public LanguageTeacher()
+        {
+            isStudent = false;
+        }
+
+        public bool Teach(LanguageStudent student, string language)
+        {
+            if (!student.isStudent)
+            {
+                return false;
+            }
+
+            var teacherKnew = this.Languages.Any(x => x.Contains(language));
+            if (!teacherKnew)
+            {
+                return false;
+            }
+
+            var studentKnew = student.Languages.Any(x => x.Contains(language));
+            if (studentKnew)
+            {
+                return false;
+            }
+
+            student.isTeach = true;
+            student.AddLanguage(language);
+            return true;
+        }
+    }
+}
---
+++ LearnLanguageTests/ProgramTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LearnLanguage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnLanguage.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void MainTest()
+        {
+            LanguageTeacher teacher1 = new LanguageTeacher();
+            LanguageTeacher teacher2 = new LanguageTeacher();
+
+            LanguageStudent student1 = new LanguageStudent();
+            LanguageStudent student2 = new LanguageStudent();
+
+            teacher1.AddLanguage("English");
+            // Normal Process
+            Assert.IsTrue(teacher1.Teach(student1, "English"));
+
+            // Student can not AddLanguage by self
+            student2.AddLanguage("English");
+            Assert.IsFalse(student2.Languages.Count > 0);
+
+            // Teacher can not Teach other Teacher
+            Assert.IsFalse(teacher1.Teach(teacher2, "English"));
+
+            // Teacher can not Teach not knew Language
+            Assert.IsFalse(teacher1.Teach(student1, "French"));
+        }
+    }
+}
